c0c5166bafe050bd9d20340c37f8e75702f3b68b
Harish Nataraj <harish.nataraj@anywhere.co>
2021-02-20 10:54:53 +0530

    category update

--- src/com/learning/spring/controllers/CategoryController.java
+++ src/com/learning/spring/controllers/CategoryController.java
@@ -1,4 +1,4 @@
-	package com.learning.spring.controllers;
+package com.learning.spring.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -12,75 +12,60 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.learning.spring.models.Category;
 import com.learning.spring.services.CategoryService;
 
 @Controller
 @RequestMapping("/category")
 public class CategoryController {
-	
+
 	private CategoryService categoryService;
-	
+
 	public void setCategoryService(CategoryService categoryService) {
 		this.categoryService = categoryService;
 	}
 
 	@GetMapping("/")
-	public String getCategoryPage(HttpSession session, Model model,HttpServletRequest request) {
-		if(session.getAttribute("userId") != null ) {
+	public String getCategoryPage(HttpSession session, Model model, HttpServletRequest request) {
+		if (session.getAttribute("userId") != null) {
 			int user_id = (int) session.getAttribute("userId");
 			model.addAttribute("category", new Category());
 			model.addAttribute("categoryList", categoryService.getAllCategories(user_id));
-			model.addAttribute("message", request.getParameter("message"));
 			return "category";
 		}
 		return "login";
 	}
-	
+
 	@PostMapping("/addCategory")
-	public String addNewCategory(@Valid @ModelAttribute("category") Category category,BindingResult bindingResult, HttpSession session, Model model) {
-		
-		if(bindingResult.hasErrors()) {
+	public String addNewCategory(@Valid @ModelAttribute("category") Category category, BindingResult bindingResult,
+			HttpSession session, RedirectAttributes redirectAttributes) {
+
+		if (bindingResult.hasErrors()) {
 			return "redirect:/category/";
 		} else {
 			int user_id = (int) session.getAttribute("userId");
-			if(categoryService.addCategory(user_id, category.getCategoryName())) {
-				model.addAttribute("message", "Category added successfully");
+			if (categoryService.addCategory(user_id, category.getCategoryName())) {
+				redirectAttributes.addFlashAttribute("message", "Category added successfully");
 			} else {
-				model.addAttribute("message", "Cannot add category");
+				redirectAttributes.addFlashAttribute("message", "Cannot add the category");
 			}
 			return "redirect:/category/";
 		}
 	}
-	
+
 	@GetMapping(value = "/deleteCategory/{categoryId}")
-	public String deleteCategory(@PathVariable("categoryId") int categoryId, Model model) {
-		
-		if(categoryService.deleteCategory(categoryId)) {
-			model.addAttribute("message", "Category deleted");
+	public String deleteCategory(@PathVariable("categoryId") int categoryId, RedirectAttributes redirectAttributes) {
+
+		if (categoryService.deleteCategory(categoryId)) {
+			redirectAttributes.addFlashAttribute("message", "Category deleted");
 		} else {
-			model.addAttribute("message", "Failed to delete Category");
+			redirectAttributes.addFlashAttribute("message", "Failed to delete Category");
 		}
-		
+
 		return "redirect:/category/";
-		
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	}
+
 }
